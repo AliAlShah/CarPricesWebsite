65a9cc8628de464e4af2bf9976e3a81a4f7783dc
AliAlShah <96441735+AliAlShah@users.noreply.github.com>
2022-11-10 17:40:53 +0000

    Scraped links to car company cars

--- main.py
+++ main.py
@@ -1,2 +1,8 @@
 from bs4 import BeautifulSoup
 import requests
+
+car_companies_url = "https://www.izmostock.com/car-stock-photos-by-brand"
+company_links_list = []
+for company_link in BeautifulSoup(requests.get(car_companies_url).text, "html.parser").find_all('div', id="by_brand"):
+    company_links_list.append(company_link.find("a", href=True)["href"])
+print(company_links_list)
